05d7e71129283fa7bf9217493e0f76c8f191207e
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-09-28 09:09:01 +0200

    starting structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Google Faqs</title>
+</head>
+<body>
+    <header>
+        <img class="header_logo" src="img/R.png" alt="logo">
+        <h3>Privacy e termini</h3>
+    </header>
+    
+</body>
+</html>
